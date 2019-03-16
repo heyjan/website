f6fc0bff04bfb51f7a52312408efc026ae25d445
heyjan <jan@heyjan.de>
2019-03-16 18:05:00 +0100

    update 0.04

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import App from './App.vue';
 import './assets/css/style.css';
 import './assets/css/bootstrap.css';
-import router from "./router";
+import router from "@/router.js";
 
 Vue.config.productionTip = false;
 
